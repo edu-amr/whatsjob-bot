4a8bf36a7101d03c8b9abbd324802f015a593ef8
Eduardo Amaro <edu.amr@hotmail.com>
2024-07-07 22:37:08 -0300

    refactor: Ajustado mensagens para  pt br

--- src/jobs/send-jobs-every-week.ts
+++ src/jobs/send-jobs-every-week.ts
@@ -7,31 +7,31 @@ async function sendBroadcastMessage(socket: WASocket) {
   const { data: numbersData, error: numbersError } = await supabaseService.from("duplicate_numeros").select("numero");
 
   if (numbersError) {
-    console.error('Error fetching numbers from the database:', numbersError);
+    console.error('Erro ao buscar números do banco de dados:', numbersError);
     return;
   }
 
   const { data: jobsData, error: jobsError } = await supabaseService.from("vagas").select("titulo, link, empresa, senioridade, modalidade");
 
   if (jobsError) {
-    console.error('Error fetching jobs from the database:', jobsError);
+    console.error('Erro ao buscar vagas do banco de dados:', jobsError);
     return;
   }
 
   if (!jobsData || jobsData.length === 0) {
-    console.log('No job available at the moment.');
+    console.log('Nenhuma vaga disponível no momento.');
     return;
   }
 
-  let message = `Here are the job openings!`;
+  let message = `Seguem as vagas!`;
 
   jobsData.forEach((job) => {
     message += 
       `\r\n\r\n` +
-      `🏢 Company: ${job.empresa}\r\n` +
-      `🎓 Seniority: ${job.senioridade}\r\n` +
-      `💼 Job: ${job.titulo}\r\n` +
-      `🚩 Modality: ${job.modalidade}\r\n` +
+      `🏢 Empresa: ${job.empresa}\r\n` +
+      `🎓 Senioridade: ${job.senioridade}\r\n` +
+      `💼 Vaga: ${job.titulo}\r\n` +
+      `🚩 Modalidade: ${job.modalidade}\r\n` +
       `🔗 Link: ${job.link}`;
   });
 
@@ -39,20 +39,19 @@ async function sendBroadcastMessage(socket: WASocket) {
     const number = entry.numero;
     try { 
       await socket?.sendMessage(number + "@s.whatsapp.net", { text: message });
-      console.log('enviei para ele ')
       delay(4000);
     } catch (sendError) {
-      console.error(`Error sending message to number ${number}:`, sendError);
+      console.error(`Erro ao enviar mensagem para o número ${number}:`, sendError);
     }
   }
 }
 
-const minutes = '*/30 * * * * *'
-const week = '0 0 9 * * 1'
+const segundos = '*/10 * * * * *'
+const semana = '0 0 9 * * 1'
 
 export async function initSendJobsEveryWeek(socket: WASocket) {
   const job = new CronJob(
-    minutes,
+    segundos,
     () => sendBroadcastMessage(socket),
     null,
     true,
