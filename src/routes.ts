bd57c984db4c910a8936e8d825caba86437f6e0d
Eduardo Amaro <edu.amr@hotmail.com>
2024-07-28 02:02:41 -0300

    refactor: Alteração das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { RenderPages } from "controllers/RenderPages";
+import { RenderPages } from "./controllers/RenderPages";
 
 const renderPages = new RenderPages()
 
